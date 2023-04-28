e7b97835ec0d181ad55efe0ad60f245e2daac3b2
vidyatiwrai <127404171+vidyatiwrai@users.noreply.github.com>
2023-04-28 17:03:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+//your JS code here. If required.
+	const browser = document.getElementById("browser-info");
+
+        let div = document.createElement("div");
+
+        let p = document.createElement("p");
+        p.innerText = "You are using "+ "  LAPTOP-5PIA4I36 "+ "version " + "  10.0.22621 N/A Build 22621"
+
+        div.append(p);
+
+        document.querySelector("#browser-info").append(div);
